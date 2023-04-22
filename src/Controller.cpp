6ed2626fa488b4b41d993c3f031312be52bc947d
Steven Baumann <stevenfbaumann@gmail.com>
2023-04-22 02:34:44 -0400

    Update Controller.cpp

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -2,25 +2,44 @@
 
 namespace sn
 {
+    // Constants for default key bindings
+    const sf::Keyboard::Key DEFAULT_A_KEY = sf::Keyboard::J;
+    const sf::Keyboard::Key DEFAULT_B_KEY = sf::Keyboard::K;
+    const sf::Keyboard::Key DEFAULT_SELECT_KEY = sf::Keyboard::RShift;
+    const sf::Keyboard::Key DEFAULT_START_KEY = sf::Keyboard::Return;
+    const sf::Keyboard::Key DEFAULT_UP_KEY = sf::Keyboard::W;
+    const sf::Keyboard::Key DEFAULT_DOWN_KEY = sf::Keyboard::S;
+    const sf::Keyboard::Key DEFAULT_LEFT_KEY = sf::Keyboard::A;
+    const sf::Keyboard::Key DEFAULT_RIGHT_KEY = sf::Keyboard::D;
+
     Controller::Controller() :
         m_keyStates(0),
         m_keyBindings(TotalButtons)
     {
-//         m_keyBindings[A] = sf::Keyboard::J;
-//         m_keyBindings[B] = sf::Keyboard::K;
-//         m_keyBindings[Select] = sf::Keyboard::RShift;
-//         m_keyBindings[Start] = sf::Keyboard::Return;
-//         m_keyBindings[Up] = sf::Keyboard::W;
-//         m_keyBindings[Down] = sf::Keyboard::S;
-//         m_keyBindings[Left] = sf::Keyboard::A;
-//         m_keyBindings[Right] = sf::Keyboard::D;
+        // Set default key bindings
+        m_keyBindings[A] = DEFAULT_A_KEY;
+        m_keyBindings[B] = DEFAULT_B_KEY;
+        m_keyBindings[Select] = DEFAULT_SELECT_KEY;
+        m_keyBindings[Start] = DEFAULT_START_KEY;
+        m_keyBindings[Up] = DEFAULT_UP_KEY;
+        m_keyBindings[Down] = DEFAULT_DOWN_KEY;
+        m_keyBindings[Left] = DEFAULT_LEFT_KEY;
+        m_keyBindings[Right] = DEFAULT_RIGHT_KEY;
     }
 
+    /**
+     * Set custom key bindings for the controller.
+     * @param keys A vector of sf::Keyboard::Key values for custom key bindings.
+     */
     void Controller::setKeyBindings(const std::vector<sf::Keyboard::Key>& keys)
     {
         m_keyBindings = keys;
     }
 
+    /**
+     * Update the internal state of the controller based on key presses.
+     * @param b A byte value representing the current state of the buttons.
+     */
     void Controller::strobe(Byte b)
     {
         m_strobe = (b & 1);
@@ -36,6 +55,10 @@ namespace sn
         }
     }
 
+    /**
+     * Read the current state of the controller.
+     * @return A byte value representing the current state of the controller.
+     */
     Byte Controller::read()
     {
         Byte ret;
